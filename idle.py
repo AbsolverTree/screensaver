f92197a4d8bf3f4e71e10e7f39b789a902f0e2ce
Gabriel Viterbo <56035644+AbsolverTree@users.noreply.github.com>
2019-10-09 09:24:06 -0300

    Add files via upload
    correções

--- idle.py
+++ idle.py
@@ -1,5 +1,6 @@
 from ctypes import Structure, windll, c_uint, sizeof, byref
 
+
 class LASTINPUTINFO(Structure):
     _fields_ = [
         ('cbSize', c_uint),
@@ -12,4 +13,3 @@ class LASTINPUTINFO(Structure):
         windll.user32.GetLastInputInfo(byref(lastInputInfo))
         millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
         return millis / 1000.0
-
--- screensaver.py
+++ screensaver.py
@@ -5,9 +5,12 @@ import sys
 import time
 
 entrys = []
+
+
 def pegar():
     entrys.append(e1.get())
     entrys.append(e2.get())
+
 
 master = tk.Tk()
 tk.Label(master, text="Endereço do vídeo:").grid(row=0)
@@ -19,33 +22,33 @@ e2 = tk.Entry(master)
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
-tk.Button(master, 
-          text='Enter', 
-          command=lambda:[pegar(),master.destroy()]).grid(row=3, 
-                                    column=0, 
-                                    sticky=tk.W, 
-                                    pady=4)
+tk.Button(master,
+          text='Enter',
+          command=lambda: [pegar(), master.destroy()]).grid(row=3,
+                                                            column=0,
+                                                            sticky=tk.W,
+                                                            pady=4)
 
-tk.Button(master, 
-          text='Exit', command=sys.exit).grid(row=3, 
-                                                       column=1, 
-                                                       sticky=tk.W, 
-                                                       pady=4)
+tk.Button(master,
+          text='Exit', command=sys.exit).grid(row=3,
+                                              column=1,
+                                              sticky=tk.W,
+                                              pady=4)
 tk.mainloop()
 
-#print(entrys)
 
 file_name = (r"a.mp4")
 cap = cv2.VideoCapture(file_name)
+
+
 def video():
     file_name = (str(entrys[0]))
     window_name = "ss"
     interframe_wait_ms = 30
 
-
-
     cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.setWindowProperty(
+        window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cap = cv2.VideoCapture(file_name)
     if not cap.isOpened():
         print("Error: Não foi possível abrir o vídeo.")
@@ -58,16 +61,15 @@ def video():
 
         cv2.imshow(window_name, frame)
         if cv2.waitKey(interframe_wait_ms) & 0x7F == ord('q'):
-            c = 1
             exit()
+
 
 mano = 0
 while True:
     time.sleep(0.001)
     mano = LASTINPUTINFO.get_idle_duration()+0.001
-    #print(mano)
     if mano > float(entrys[1])*60:
         video()
     else:
-       cap.release()
-       cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
